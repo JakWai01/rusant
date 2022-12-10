10c8805aab5d9aa9b1421095b0c31c6f4d1c0ffc
JakWai01 <jakob.waibel@web.de>
2022-12-10 11:23:02 +0100

    feat: Use `gtk4paintablesink` instead of `autovideosink`

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
-mod sender;
 mod receiver;
+mod sender;
 
-use sender::Sender;
 use receiver::Receiver;
+use sender::Sender;
 
-use gtk::glib;
 use config::Config;
-use std::path::Path;
+use gtk::glib;
 use std::collections::HashMap;
 use std::env;
+use std::path::Path;
 
 fn main() {
     println!("Hello World!");
-    
+
     let args: Vec<String> = env::args().collect();
 
     // Parse config
     let config = Config::builder()
-    .add_source(config::File::with_name("Config"))
-    .build()
-    .unwrap()
-    .try_deserialize::<HashMap<String, String>>()
-    .unwrap();
+        .add_source(config::File::with_name("Config"))
+        .build()
+        .unwrap()
+        .try_deserialize::<HashMap<String, String>>()
+        .unwrap();
 
     let name = &config.get("name").unwrap();
     let app_id = &config.get("app_id").unwrap();
-    
+
     // Check if video device exists
     if !Path::new("/dev/video0").exists() {
         panic!("No webcam detected: /dev/video0 cannot be found.")
     }
-    
+
     // Initialize GTK
     gtk::init().unwrap_or_else(|_| panic!("Failed to initialize GTK."));
 
@@ -42,36 +42,34 @@ fn main() {
     // Apply configuration
     glib::set_application_name(name);
     gtk::Window::set_default_icon_name(app_id);
- 
-    // let app = gtk::Application::new(None, gio::ApplicationFlags::FLAGS_NONE);
 
     match args.len() {
         // no arguments passed
         1 => {
-            panic!("Please specify if you want to run the application as a 'sender' or a 'receiver'");
-        },
+            let receiver = receiver::ReceiverPipeline {};
+            receiver.receive();
+        }
         2 => {
             match args[1].as_str() {
                 "sender" => {
-                    let sender = sender::SenderPipeline{};
+                    let sender = sender::SenderPipeline {};
                     sender.send();
-                },
+                }
                 "receiver" => {
-                    let receiver = receiver::ReceiverPipeline{};
-                    receiver.receive();
-                },
+                    /*
+                    You can't provide args when working with gdk as it suspects
+                    that this will be a filename and this would require additional configuration
+                    */
+                }
                 _ => {
                     panic!("Invalid argument!")
                 }
             }
-
-        },
+        }
         _ => {
             println!("Too many arguments passed!")
         }
     }
-    // app.connect_activate(build_ui);
-    // app.run();
 
     unsafe {
         gst::deinit();
--- src/receiver/mod.rs
+++ src/receiver/mod.rs
@@ -1,65 +1,119 @@
 use gst::prelude::*;
+use gtk::prelude::*;
+use gtk::{gdk, gio, glib};
+use std::cell::RefCell;
 
 pub trait Receiver {
-    fn receive(&self){}
+    fn receive(&self) {}
 }
 
-pub struct ReceiverPipeline{}
+pub struct ReceiverPipeline {}
 
 impl Receiver for ReceiverPipeline {
-    fn receive(&self){
+    fn receive(&self) {
         println!("Client: Hello!");
-        
-        // Initialize pipeline
-        let pipeline = gst::Pipeline::new(None);
 
-        // Initialize pads
-        let src = gst::ElementFactory::make("udpsrc")
-            .property("address", "127.0.0.1")
-            .property("port", 5200)
-            .build()
-            .unwrap();
-        let filter = gst::ElementFactory::make("capsfilter").build().unwrap();
-        let rtpjpegdepay = gst::ElementFactory::make("rtpjpegdepay").build().unwrap();
-        let jpegdec = gst::ElementFactory::make("jpegdec").build().unwrap();
-        let sink = gst::ElementFactory::make("autovideosink").build().unwrap();
+        gtk::init().unwrap();
 
-        let caps = gst::Caps::new_simple(
-            "application/x-rtp",
-            &[("encoding-name", &"JPEG"), ("payload", &26i32)],
-        );
-        filter.set_property("caps", &caps);
+        gstgtk4::plugin_register_static().expect("Failed to register gstgtk4 plugin");
 
-        // Add pads
-        pipeline
-            .add_many(&[&src, &filter, &rtpjpegdepay, &jpegdec, &sink])
-            .unwrap();
+        {
+            let app = gtk::Application::new(None, gio::ApplicationFlags::FLAGS_NONE);
 
-        // Link pads
-        gst::Element::link_many(&[&src, &filter, &rtpjpegdepay, &jpegdec, &sink]).unwrap();
-
-        // Start pipeline
-        pipeline.set_state(gst::State::Playing).unwrap();
-
-        let bus = pipeline
-            .bus()
-            .expect("Pipeline without bus. Shouldn't happend!");
-
-        // Listen on bus for events
-        for msg in bus.iter_timed(gst::ClockTime::NONE) {
-            use gst::MessageView;
-
-            match msg.view() {
-                MessageView::Eos(..) => break,
-                MessageView::Error(_err) => {
-                    pipeline.set_state(gst::State::Null).unwrap();
-                }
-                _ => (),
-            }
+            app.connect_activate(build_ui);
+            app.run();
         }
-
-        // Stop pipeline
-        pipeline.set_state(gst::State::Null).unwrap();
-
     }
+}
+
+fn build_ui(app: &gtk::Application) {
+    // Initialize pipeline
+    let pipeline = gst::Pipeline::new(None);
+
+    // Initialize pads
+    let src = gst::ElementFactory::make("udpsrc")
+        .property("address", "127.0.0.1")
+        .property("port", 5200)
+        .build()
+        .unwrap();
+    let filter = gst::ElementFactory::make("capsfilter").build().unwrap();
+    let rtpjpegdepay = gst::ElementFactory::make("rtpjpegdepay").build().unwrap();
+    let jpegdec = gst::ElementFactory::make("jpegdec").build().unwrap();
+    let sink = gst::ElementFactory::make("gtk4paintablesink").build().unwrap();
+
+    let caps = gst::Caps::new_simple(
+        "application/x-rtp",
+        &[("encoding-name", &"JPEG"), ("payload", &26i32)],
+    );
+    filter.set_property("caps", &caps);
+
+    let paintable = sink.property::<gdk::Paintable>("paintable");
+
+    // Add pads
+    pipeline
+        .add_many(&[&src, &filter, &rtpjpegdepay, &jpegdec, &sink])
+        .unwrap();
+
+    // Link pads
+    gst::Element::link_many(&[&src, &filter, &rtpjpegdepay, &jpegdec, &sink]).unwrap();
+
+    let window = gtk::ApplicationWindow::new(app);
+
+    window.set_default_size(1280, 720);
+
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    let picture = gtk::Picture::new();
+
+    picture.set_paintable(Some(&paintable));
+    vbox.append(&picture);
+
+    window.set_child(Some(&vbox));
+    window.show();
+
+    app.add_window(&window);
+
+    let bus = pipeline.bus().unwrap();
+
+    // Start pipeline
+    pipeline.set_state(gst::State::Playing).unwrap();
+
+    let app_weak = app.downgrade();
+
+    bus.add_watch_local(move |_, msg| {
+        use gst::MessageView;
+
+        let app = match app_weak.upgrade() {
+            Some(app) => app,
+            None => return glib::Continue(false),
+        };
+
+        match msg.view() {
+            MessageView::Eos(..) => app.quit(),
+            MessageView::Error(err) => {
+                println!(
+                    "Error from {:?}: {} ({:?})",
+                    err.src().map(|s| s.path_string()),
+                    err.error(),
+                    err.debug()
+                );
+                app.quit();
+            }
+            _ => (),
+        };
+
+        glib::Continue(true)
+    })
+    .expect("Failed to add bus watch");
+
+    let pipeline = RefCell::new(Some(pipeline));
+    app.connect_shutdown(move |_| {
+        window.close();
+
+        if let Some(pipeline) = pipeline.borrow_mut().take() {
+            pipeline
+                .set_state(gst::State::Null)
+                .expect("Unable to set the pipeline to the `Null` state");
+            pipeline.bus().unwrap().remove_watch().unwrap();
+        }
+    });
 }
