6e98ade1325a7ee59a20e234b369d496bb0f4805
JakWai01 <jakob.waibel@web.de>
2023-01-18 21:41:55 +0100

    feat: Add sample bindings to libsodium

--- rusant-sys/build.rs
+++ rusant-sys/build.rs
@@ -1,3 +1,28 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Building...");
+    pkg_config::Config::new()
+        .print_system_libs(false)
+        .atleast_version("1.0.18")
+        .probe("libsodium")
+        .unwrap();
+
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .allowlist_function("sodium_init")
+        .allowlist_function("crypto_generichash")
+        .allowlist_var("crypto_generichash_.*")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
--- rusant-sys/lib.rs
+++ rusant-sys/lib.rs
@@ -1,3 +1,67 @@
+#![feature(maybe_uninit_slice)]
+
+use std::mem::MaybeUninit;
+
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[allow(dead_code)]
+mod ffi {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+#[non_exhaustive]
+#[derive(Clone, Debug)]
+pub struct Sodium;
+
+impl Sodium {
+    pub fn new() -> Result<Self, ()> {
+        if unsafe { ffi::sodium_init() } < 0 {
+            Err(())
+        } else {
+            Ok(Self)
+        }
+    }
+
+    pub fn cryptogenerichash<'a>(
+        &self,
+        input: &[u8],
+        key: Option<&[u8]>,
+        out: &'a mut [MaybeUninit<u8>],
+    ) -> Result<&'a mut [u8], ()> {
+        assert!(out.len() >= usize::try_from(ffi::crypto_generichash_BYTES_MIN).unwrap());
+        assert!(out.len() <= usize::try_from(ffi::crypto_generichash_BYTES_MAX).unwrap());
+        if let Some(key) = key {
+            assert!(key.len() >= usize::try_from(ffi::crypto_generichash_KEYBYTES_MIN).unwrap());
+            assert!(key.len() <= usize::try_from(ffi::crypto_generichash_KEYBYTES_MAX).unwrap());
+        }
+        let (key, keylen) = if let Some(key) = key {
+            (key.as_ptr(), key.len())
+        } else {
+            (std::ptr::null(), 0)
+        };
+        // SAFETY: We've checked the requirements of the function (MIN/MAX) and the
+        // presence of the function guarantees that init has been called
+        let res = unsafe {
+            ffi::crypto_generichash(
+                MaybeUninit::slice_as_mut_ptr(out),
+                out.len(),
+                input.as_ptr(),
+                input.len() as u64,
+                key,
+                keylen,
+            )
+        };
+
+        if res < 0 {
+            return Err(());
+        }
+
+        // SAFETY: crypto_generichash_writes_to (and thus initializes) all the bytes of out
+        Ok(unsafe {MaybeUninit::slice_assume_init_mut(out)})
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -8,7 +72,16 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        Sodium::new().unwrap();
+    }
+    
+    #[test]
+    fn it_hashes() {
+        let s = Sodium::new().unwrap();
+        let mut out = [MaybeUninit::uninit(); ffi::crypto_generichash_BYTES as usize];
+        let bytes = s.cryptogenerichash(b"Arbitrary data to hash in Rust", None, &mut out)
+        .unwrap();
+
+        println!("{}", hex::encode(&bytes));
     }
 }
