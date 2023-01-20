99d0f2ea10cb5646269c52011ae581870c8e467c
JakWai01 <jakob.waibel@web.de>
2023-01-20 19:31:07 +0100

    feat: Add closure example

--- rusant-shared/lib.rs
+++ rusant-shared/lib.rs
@@ -26,27 +26,54 @@ unsafe extern "C" fn add_result_to_total(
     *total += result;
 }
 
+unsafe extern "C" fn trampoline<F>(result: c_int, user_data: *mut c_void) where F: FnMut(c_int) {
+    let user_data = &mut *(user_data as *mut F);
+    user_data(result);
+}
+
+pub fn get_trampoline<F>(_closure: &F) -> shared::AddCallback where F: FnMut(c_int) {
+    Some(trampoline::<F>)
+}
+
 pub fn badd() {
-    let numbers = [1, 2, 3, 4, 5, 6, 7];
-    let mut total = 0;
+    // let numbers = [1, 2, 3, 4, 5, 6, 7];
+    // let mut total = 0;
 
-    for i in 0..numbers.len() {
-        for j in i..numbers.len() {
-            let a = numbers[i];
-            let b = numbers[j];
+    // for i in 0..numbers.len() {
+    //     for j in i..numbers.len() {
+    //         let a = numbers[i];
+    //         let b = numbers[j];
 
-            unsafe {
-                shared::better_add_two_numbers(
-                    a,
-                    b,
-                    Some(add_result_to_total),
-                    &mut total as *mut c_int as *mut c_void,
-                );
-            }
+    //         unsafe {
+    //             shared::better_add_two_numbers(
+    //                 a,
+    //                 b,
+    //                 Some(add_result_to_total),
+    //                 &mut total as *mut c_int as *mut c_void,
+    //             );
+    //         }
+    //     }
+    // }
+
+    // println!("The sum is {}", total);
+    let mut got = 0;
+
+    {
+        let mut closure = |result: c_int| got = result;
+        let trampoline = get_trampoline(&closure);
+
+        unsafe {
+            shared::better_add_two_numbers(
+                1,
+                2,
+                trampoline,
+                &mut closure as *mut _ as *mut c_void,
+            );
         }
     }
 
-    println!("The sum is {}", total);
+    println!("got {}", got);
+
 }
 
 #[cfg(test)]
